7b4e7c329352fc27abfb9c9bb6be3a950ab94069
Nancy Chauhan <nancychn1@gmail.com>
2019-01-02 16:53:26 +0530

    Update edid_rom.vhd

--- edid_rom.vhd
+++ edid_rom.vhd
@@ -1,33 +1,4 @@
-----------------------------------------------------------------------------------
--- Engineer: Mike Field <hamster@snap.net.nz>
---
--- Module Name: edid_rom - Behavioral
---
--- Description: A simple EDID ROM, configured for 1920x1080@60Hz, HDMI format.
---
-----------------------------------------------------------------------------------
--- The MIT License (MIT)
--- 
--- Copyright (c) 2015 Michael Alan Field
--- 
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
--- 
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
--- 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
--- THE SOFTWARE.
-----------------------------------------------------------------------------------
+-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
